e6adfa2423dfa21d106b63d3f8a1279e41319d1f
jackbarrely <darrylheyward89@gmail.com>
2024-12-16 15:05:17 +0900

    update:

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,27 @@
-class Person:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
+# class person:
+#   def __init__(self, fname, lname):
+#     self.firstname = fname
+#     self.lastname = lname
 
-  def myfunc(w):
-    print("Hello my name is " + w.name)
+#   def printname(self):
+#     print(self.firstname, self.lastname)
 
-p1 = Person("John", 36)
-p1.myfunc()
+# class Student(person):
+#   pass
+# x = Student("Mike", "Olsen")
+# x.printname()
+
+
+class person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(person):
+  def __init__(self, fname, lname):
+    person.__init__(self, fname, lname)
+
+x = Student("Mike", "Olsen")
+x.printname()
